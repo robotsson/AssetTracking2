a48abae7768dc4425ed72ef83418d684a7c82031
github.com/robotsson <game-baked-jogging@duck.com>
2024-12-05 11:47:19 +0100

    DataSeed

--- AssetContext.cs
+++ AssetContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.SqlClient;
+using System.Data.Common;
 
 namespace AssetTrackerEF
 {
@@ -25,11 +26,38 @@ namespace AssetTrackerEF
                 InitialCatalog = "AssetTracker",
                 TrustServerCertificate = true
             };
-        
-            OptionsBuilder.UseSqlServer(builder.ConnectionString);
+            
+            string ConnectionString = builder.ConnectionString;        
+            // string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=efdb1;Integrated Security=True;";
+
+            OptionsBuilder.UseSqlServer(ConnectionString);
         }
-        
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Data Seeding
+
+            modelBuilder.Entity<Computer>().HasData(
+                new Computer { Id = 1, Brand = "HP", Model = "Elitebook", Price = 1176.03m, Currency = "EUR", DatePurchased = new DateOnly( 2019, 6, 1 ), Office = "Spain" },
+                new Computer { Id = 3, Brand = "Asus", Model = "W234", Price = 4900m, Currency = "SEK", DatePurchased = new DateOnly( 2020, 10, 2 ), Office = "Sweden" },
+                new Computer { Id = 4, Brand = "Lenovo", Model = "Yoga 730", Price = 835m, Currency = "USD", DatePurchased = new DateOnly( 2018, 5, 28 ), Office = "USA" },
+                new Computer { Id = 7, Brand = "Lenovo", Model = "Yoga 530", Price = 1030m, Currency = "USD", DatePurchased = new DateOnly( 2019, 5, 21 ), Office = "USA" },
+                new Computer { Id = 10, Brand = "Apple", Model = "Macbook Pro", Price = 970m, Currency = "EUR", DatePurchased = new DateOnly( 2022, 7, 13 ), Office = "Spain" },
+                new Computer { Id = 11, Brand = "Asus", Model = "ROG 500", Price = 9999.90m, Currency = "SEK", DatePurchased = new DateOnly( 2024, 10, 15 ), Office = "Sweden" }
+            );
+
+            modelBuilder.Entity<Phone>().HasData(
+                new Phone { Id = 2, Brand = "Apple", Model = "Iphone 15", Price = 10000m, Currency = "USD", DatePurchased = new DateOnly( 2024, 9, 11 ), Office = "USA" },
+                new Phone { Id = 5, Brand = "Apple", Model = "iPhone", Price = 818.18m, Currency = "EUR", DatePurchased = new DateOnly( 2020, 9, 25 ), Office = "Spain" },
+                new Phone { Id = 6, Brand = "Apple", Model = "iPhone", Price = 10375m, Currency = "SEK", DatePurchased = new DateOnly( 2018, 7, 15 ), Office = "Sweden" },
+                new Phone { Id = 8, Brand = "Motorola", Model = "Razr", Price = 6083.33m, Currency = "SEK", DatePurchased = new DateOnly( 2022, 5, 16 ), Office = "Sweden" },
+                new Phone { Id = 9, Brand = "Samsung", Model = "Galaxy S23", Price = 8083.33m, Currency = "SEK", DatePurchased = new DateOnly( 2023, 3, 16 ), Office = "Sweden" },
+                new Phone { Id = 12, Brand = "Nokia", Model = "3310", Price = 160.11m, Currency = "EUR", DatePurchased = new DateOnly( 2019, 5, 16 ), Office = "Germany" },
+                new Phone { Id = 13, Brand = "Xiaomi", Model = "14 Ultra", Price = 808.08m, Currency = "EUR", DatePurchased = new DateOnly( 2023, 3, 16 ), Office = "France" } 
+            );
+
+        }
     }
-
 }
---
+++ Migrations/20241205101952_DataSeed.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AssetTrackerEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Assets",
+                columns: new[] { "Id", "Brand", "Currency", "DatePurchased", "Discriminator", "Model", "Office", "Price" },
+                values: new object[,]
+                {
+                    { 1, "HP", "EUR", new DateOnly(2019, 6, 1), "Computer", "Elitebook", "Spain", 1176.03m },
+                    { 2, "Apple", "USD", new DateOnly(2024, 9, 11), "Phone", "Iphone 15", "USA", 10000m },
+                    { 3, "Asus", "SEK", new DateOnly(2020, 10, 2), "Computer", "W234", "Sweden", 4900m },
+                    { 4, "Lenovo", "USD", new DateOnly(2018, 5, 28), "Computer", "Yoga 730", "USA", 835m },
+                    { 5, "Apple", "EUR", new DateOnly(2020, 9, 25), "Phone", "iPhone", "Spain", 818.18m },
+                    { 6, "Apple", "SEK", new DateOnly(2018, 7, 15), "Phone", "iPhone", "Sweden", 10375m },
+                    { 7, "Lenovo", "USD", new DateOnly(2019, 5, 21), "Computer", "Yoga 530", "USA", 1030m },
+                    { 8, "Motorola", "SEK", new DateOnly(2022, 5, 16), "Phone", "Razr", "Sweden", 6083.33m },
+                    { 9, "Samsung", "SEK", new DateOnly(2023, 3, 16), "Phone", "Galaxy S23", "Sweden", 8083.33m },
+                    { 10, "Apple", "EUR", new DateOnly(2022, 7, 13), "Computer", "Macbook Pro", "Spain", 970m },
+                    { 11, "Asus", "SEK", new DateOnly(2024, 10, 15), "Computer", "ROG 500", "Sweden", 9999.90m },
+                    { 12, "Nokia", "EUR", new DateOnly(2019, 5, 16), "Phone", "3310", "Germany", 160.11m },
+                    { 13, "Xiaomi", "EUR", new DateOnly(2023, 3, 16), "Phone", "14 Ultra", "France", 808.08m }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Assets",
+                keyColumn: "Id",
+                keyValue: 13);
+        }
+    }
+}
